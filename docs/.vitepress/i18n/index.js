5f768c0d7691e73530b0ae1d1970b2923ab1f78f
Devnol <pgkoutsoumanis@gmail.com>
2025-11-21 10:52:57 +0200

    i18n: Remove other language strings
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- docs/.vitepress/i18n/index.js
+++ docs/.vitepress/i18n/index.js
@@ -1,5 +1,3 @@
 import en_US from './en_US'
-import hu_HU from './hu_HU'
-import ko_KR from './ko_KR'
 
-export { en_US, hu_HU, ko_KR }
+export {en_US}
--- docs/.vitepress/i18n/strings/index.js
+++ docs/.vitepress/i18n/strings/index.js
@@ -1,5 +1,3 @@
 import en_US from './en_US' with { type: 'json' }
-import hu_HU from './hu_HU' with { type: 'json' }
-import ko_KR from './ko_KR' with { type: 'json' }
 
-export { en_US, hu_HU, ko_KR }
+export {en_US}
